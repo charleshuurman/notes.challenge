448543e6e4d8eb6adf638026d3f027c125ea7dfb
Charles Huurman <ch@utopian.ink>
2023-10-24 19:35:26 -0500

    “server.js_creation”

---
+++ Develop/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+// routes will go here
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on PORT ${PORT}`);
+});
