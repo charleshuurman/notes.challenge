17705a03c54ffdd483059c0c3c868963c9fe008b
Charles Huurman <ch@utopian.ink>
2023-10-24 19:46:11 -0500

    “API_Routes”

--- Develop/server.js
+++ Develop/server.js
@@ -12,23 +12,45 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-// routes will go here
-
-app.listen(PORT, () => {
-  console.log(`Server is listening on PORT ${PORT}`);
-});
-
 // Setting Up Routes
 // HTML routes
 
-// Route to serve notes.html
+// Route to notes.html
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
   });
   
-// Route to serve index.html
+// Route to index.html
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
   });
   
+//API Routes
+// Route to get all notes
+app.get('/api/notes', (req, res) => {
+    fs.readFile(path.join(__dirname, '/db/db.json'), 'utf8', (err, data) => {
+      if (err) throw err;
+      res.json(JSON.parse(data));
+    });
+  });
   
+  // Route to add a new note
+  app.post('/api/notes', (req, res) => {
+    const newNote = req.body;
+    newNote.id = uuidv4(); // Assign a unique ID to the note
+  
+    fs.readFile(path.join(__dirname, '/db/db.json'), 'utf8', (err, data) => {
+      if (err) throw err;
+      const notes = JSON.parse(data);
+      notes.push(newNote);
+  
+      fs.writeFile(path.join(__dirname, '/db/db.json'), JSON.stringify(notes), (err) => {
+        if (err) throw err;
+        res.json(newNote);
+      });
+    });
+  });
+  
+  app.listen(PORT, () => {
+    console.log(`Server is listening on PORT ${PORT}`);
+  });
